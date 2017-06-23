d36edc4cd63296c33c58c0570fd8a1c809ddd7c7
gbarcelo <gbarcelo@users.noreply.github.com>
2017-06-22 16:45:51 -1000

    test comment

--- app/src/main/java/com/example/watchman/MainActivity.java
+++ app/src/main/java/com/example/watchman/MainActivity.java
@@ -65,5 +65,5 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
+//hey ben
 }
