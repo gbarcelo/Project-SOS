94fa6faba6d6878a71f5bbcba1841bd2f78d13dd
gbarcelo <gbarcelo@users.noreply.github.com>
2017-06-22 16:28:27 -1000

    created Hello World Dialog on buttonMain press

--- app/src/main/java/com/example/watchman/fragMain.java
+++ app/src/main/java/com/example/watchman/fragMain.java
@@ -1,15 +1,18 @@
 package com.example.watchman;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import static com.example.watchman.R.id.buttonMain;
 
 public class fragMain extends Fragment {
-
 
     public fragMain() {
         // Required empty public constructor
@@ -20,7 +23,22 @@ public class fragMain extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_frag_main, container, false);
+        Button mainb = rootView.findViewById(buttonMain);
+
+        mainb.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                MessageBox("Hello World"); //nothing function
+            }
+        });
+        return rootView;
+    }
+
+    public void MessageBox(String message)
+    {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
 
 }
